c5b8e977797e625a14a5ee1e9a6115ec0bf34ce7
gevdev <gevorg.aghabekov@gmail.com>
2016-01-24 12:36:42 -0800

    I was told to do this

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@
  * Config var for app
 **/
 module.exports = {
-  mongoDBURI: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test',
+  mongoDBURI: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/db',
   port: process.env.PORT || 4941,
   secret: process.env.SECRET || 'mysecret'
 };
