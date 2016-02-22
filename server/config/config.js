8a7d01789ec6241bf8df6dfb9626bc6526ce6c87
Serhiy Vorobyov <serhiyvorobyov@users.noreply.github.com>
2016-02-21 20:04:40 -0800

    default config points to our MongoLab mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@
  * Config var for app
 **/
 module.exports = {
-  mongoDBURI: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/db',
+  mongoDBURI: process.env.MONGOLAB_URI || 'mongodb://heroku_w9bxpzpc:q7po0h9hcnap9vkcem34givjr4@ds061335.mongolab.com:61335/heroku_w9bxpzpc',
   port: process.env.PORT || 4941,
   secret: process.env.SECRET || 'mysecret'
 };
