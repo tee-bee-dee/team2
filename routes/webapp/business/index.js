525fa038ce76834dbab7481002aa994f122af3be
Serhiy Vorobyov <serhiy.vorobyov@gmail.com>
2016-03-04 22:50:04 -0800

    deleted all unnecessary dependancies within index.js that had to do with form creation as well as removing the separate files of theme and logo upload, renamed webapp business files with camelcase

--- routes/webapp/business/index.js
+++ routes/webapp/business/index.js
@@ -1,31 +1,23 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
-
-//Define the controllers for checkin process
+// Define the routes for navigating the dashboard web application
 var landing = require('./landing');
-var theming = require('./theming');
 var login = require('./login');
-var formbuilder = require('./formbuilder');
-var accountSettings = require('./accountsettings');
-var uploadLogo = require('./uploadlogo');
+var accountSettings = require('./accountSettings');
 var register = require('./register');
 var dashboard = require('./dashboard');
-var addEmployees = require('./addemployees');
-var employeeRegister = require('./employeeregister');
-var viewForm = require('./viewform');
-var customizeTheme = require('./customize_theme');
-var manageForms = require('./manage_forms');
-var businesssetting = require('./businesssetting');
-var adminDashboard = require('./dashboard-admin');
-var visitorList = require('./visitor-list');
+var addEmployees = require('./addEmployees');
+var employeeRegister = require('./employeeRegister');
+var businesssetting = require('./businessSetting');
+var visitorList = require('./visitorList');
 
+/*
+ * TODO: Explain where this export is pointing to.
+ */
 module.exports = function (passport) {
-
-
-
-    //Pass in passport
-
     //Setup the routes
     router.get('/', landing.get);
     router.post('/', landing.post);
@@ -33,14 +25,11 @@ module.exports = function (passport) {
     router.get('/theming', isLoggedInBusiness, theming.get);
 
     router.get('/login', login.get);
-    router.post('/login',passport.authenticate('local-login',{
+    router.post('/login', passport.authenticate('local-login',{
         successRedirect : '/dashboard',
         failureRedirect : '/login',
         failureFlash: true
     }));
-
-    router.get('/formbuilder',isLoggedIn, formbuilder.get);
-
 
     router.get('/accountSettings', isLoggedIn, accountSettings.get);
     router.post('/accountSettings', isLoggedIn, accountSettings.post);
@@ -49,7 +38,7 @@ module.exports = function (passport) {
     router.post('/businesssetting', isLoggedInBusiness,businesssetting.post);
 
     router.get('/register', register.get);
-    router.post('/register',passport.authenticate('local-signup',{
+    router.post('/register', passport.authenticate('local-signup', {
         successRedirect : '/dashboard', // redirect to the secure profile section
         failureRedirect : '/register' // redirect back to the signup page if there is an error
     }));
@@ -57,22 +46,16 @@ module.exports = function (passport) {
     router.get('/dashboard', isLoggedIn, dashboard.get);
     router.get('/visitor-list', isLoggedIn, visitorList.get);
 
-    router.get('/addemployees',isLoggedInBusiness, addEmployees.get);
-    router.post('/addemployees',isLoggedInBusiness, addEmployees.post);
-
-    //router.get('/customizetheme', isLoggedIn, customizeTheme.get);
-
-    router.get('/manageforms', isLoggedInBusiness, manageForms.get);
+    router.get('/addemployees', isLoggedInBusiness, addEmployees.get);
+    router.post('/addemployees', isLoggedInBusiness, addEmployees.post);
 
     router.get('/employeeregister', employeeRegister.get);
-    router.post('/employeeregister', passport.authenticate('local-signup-employee',{
+    router.post('/employeeregister', passport.authenticate('local-signup-employee', {
         successRedirect : '/dashboard', // redirect to the secure profile section
         failureRedirect : '/register' // redirect back to the signup page if there is an error
     }));
 
-    router.get('/viewform/:id', viewForm.get);
-
-function isLoggedIn(req,res,next){
+    function isLoggedIn(req,res,next){
         if(req.isAuthenticated()){
             return next();
         }
@@ -80,16 +63,16 @@ function isLoggedIn(req,res,next){
         res.redirect('/');
     }
 
-// route middleware to make sure a user is logged in
-function isLoggedInBusiness(req, res, next) {
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated()&& (req.user[0].admin === true)){
-        return next();
+    // route middleware to make sure a user is logged in
+    function isLoggedInBusiness(req, res, next) {
+        // if user is authenticated in the session, carry on
+        if (req.isAuthenticated()&& (req.user[0].admin === true)){
+            return next();
+        }
+        req.flash("permission", "You do not have permission to access that page");
+        // if they aren't redirect them to the home page
+        res.redirect('back');
     }
-    req.flash("permission", "You do not have permission to access that page");
-    // if they aren't redirect them to the home page
-    res.redirect('back');
-}
 
 
     return router;
