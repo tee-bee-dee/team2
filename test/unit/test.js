eb0df77738f3cceaafaa240c358e23bf69f75832
Gabriel Rangel <glrangel25@gmail.com>
2017-05-15 18:41:26 -0700

    Added simple unit test

--- test/unit/test.js
+++ test/unit/test.js
@@ -1,5 +1,23 @@
-describe ("test", function(){
-	it("should work", function () {
-		expect(true).toBe(true)
-	})
-})
+var assert = require("assert"); 
+
+
+// describe('Login Functionality', function() {
+//   describe('#validateLogin()', function() {
+//     it('Verifies that a login is valid before calling a function', function() {
+//       var auth = require('../../lib/auth.js');
+
+//       	assert.equal(auth.validateLogin(db,"test@email.com","1234",test), true);
+//     	});
+// 	});
+// });
+
+
+describe('rgb object to CSS ', function() {
+  describe('#rgbObjectToCSS()', function() {
+    it('Convert passed in object to CSS', function() {
+      var style = require('../../lib/style.js');
+      var obj = {r:100,g:200,b:25,a:1}
+      	assert.equal(style.rgbObjectToCSS(obj),'rgba(100,200,25,1)');
+    	});
+	});
+});
