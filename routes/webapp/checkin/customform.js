839ad236a9c9d85e9acf5b0c9759760f67c16ec9
Kevin Wong <kevinwongwc95@gmail.com>
2017-06-02 00:35:24 -0700

    add docs

--- routes/webapp/checkin/customform.js
+++ routes/webapp/checkin/customform.js
@@ -1,53 +1,232 @@
-var ObjectId = require('mongodb').ObjectID;
+var ObjectID = require('mongodb').ObjectID;
 var _ = require('underscore');
 var style = require('./../../../lib/style.js');
+var sms = require('./../../../lib/sms.js');
+
+/**
+ * @api {render} customform.makeDropdown
+ * @apiName makeDropdown
+ * @apiGroup Create Form
+ * @apiDescription Creates a dropdown menu based on templates
+ *
+ *
+ * @apiParam {List} options of forms to add to the dropdown menu
+ * @apiParam {string} name of group of forms to create as
+ * @apiParam {string} form body (used to prefill form)
+ */
+
+function makeDropdown(options, name, body) {
+    var s = '<select class="form-control" name="'+name+'" id="'+name+'">';
+    _.each(options, function (option) {
+        s += '<option value="'+option+'" ' + (body[name] === option ? 'selected' : '') + '>'+option+'</option> ';
+    });
+    s+= '</select>';
+    return s;
+}
+
+/**
+ * @api {render} customform.makeDropdown Enter form text
+ * @apiName Create text field
+ * @apiGroup Create Form
+ * @apiDescription Create form html tag with body and title name
+ *
+ * @apiParam {String} name of the input tag
+ * @apiParam {string} body
+ *
+ */
+
+function makeTextfield(name, body) {
+    return'<input type="text" class="form-control form-width-custom" name="'+name+'" id="' + name + '"value="' + (body[name] || '') + '">';
+}
+
+function makeFormGroup(field, index, body) {
+    var name = '_' + index;
+
+    var s = '<div class="form-group">';
+    s += '<label for="' + name + '" class="col-md-4 control-label">' + field.label + '</label>';
+
+    s += '<div class="col-md-8">';
+    if (field.type === 'textfield') {
+        s += makeTextfield(name, body);
+    } else if (field.type === 'dropdown') {
+        s += makeDropdown(field.options, name, body);
+    }
+    s += '</div>';
+
+    s += '</div>';
+    return s;
+}
+
+/**
+ * @api {render} customform.makeDropdown
+ * @apiName Create Submit Button
+ * @apiGroup Create Form
+ * @apiDescription Create form html tag with body and title name
+ *
+ */
+
+function makeSubmitButton() {
+    var s = '<div class="form-group">';
+    s += '<div>';
+    s += '<button type="submit" class="btn btn-lg btn-block custom-button custom-center">Submit</button>';
+    s += '</div>';
+    s += '</div>';
+
+    return s;
+}
+/**
+ * @api {render} customform.makeDropdown
+ * @apiName makeForm
+ * @apiGroup Create Form
+ * @apiDescription Find the appropriate form for the given business and create
+ *
+ * @apiParam db The MongoDB object
+ * @apiParam businessId The id for the business
+ * @apiParam body The form body (used to prefill form)
+ * @apiParam fn Callback function `fn(formHtml)`
+ */
+function makeForm(db, businessId, body, fn) {
+    var businesses = db.get('businesses');
+    var forms = db.get('forms');
+
+    //Find the business and get their form
+    businesses.findById(businessId, function (err, business) {
+        if (err) {
+            return fn(err);
+        }
+        if (!business) {
+            return fn(new Error('Business not found: ' + businessId));
+        }
+        forms.findOne({business: business._id}, function (err, form) {
+            if (err) {
+                return fn(err);
+            }
+            if (!form) {
+                return fn(new Error('Form not found for business: ' + businessId));
+            }
+
+            var formHtml = '<form class="form-horizontal" method="post" action="customform" enctype="application/x-www-form-urlencoded">';
+
+            _.each(form.fields, function (field, index) {
+                formHtml += makeFormGroup(field, index, body);
+            });
+
+            formHtml += makeSubmitButton();
+
+            formHtml += '</form>';
+
+            fn(null, formHtml);
+        });
+    });
+}
 
 exports.get = function (req, res, next) {
     var business = req.session.business;
-    var db = req.db;
-    var forms = req.db.get('forms');
-
-    var businessId = business._id;
-
-    forms.find({ business: businessId }, function(err, result) {
-      var form = result[0].form;
-      if (!form) return res.redirect('sign');
-      res.render('checkin/customform', {
-          form: form,
-          companyName: business.companyName,
-          bg: business.style.bg,
-          logo: business.logo,
-          layout: false
-      });
+    makeForm(req.db, req.params.id, {}, function (err, formHtml) {
+        if (err) {
+            return next(err);
+        }
+        res.render('checkin/customform', {
+            formHtml: formHtml,
+            companyName: business.companyName,
+            bg: business.style.bg,
+            logo: business.logo,
+            buttonBg: style.rgbObjectToCSS(business.style.buttonBg),
+            buttonText: style.rgbObjectToCSS(business.style.buttonText),
+            containerText: style.rgbObjectToCSS(business.style.containerText),
+            containerBg: style.rgbObjectToCSS(business.style.containerBg)
+        });
     });
 };
 
 exports.post = function (req, res, next) {
     var db = req.db;
-    var appointments = db.get('appointments');
     var forms = db.get('forms');
 
-    var formResponse = [];
-
-    var appointmentId = req.session.appointmentId;
     var business = req.session.business;
 
-    forms.find({ business: business._id }, function(err, result) {
-      var form = JSON.parse(result[0].form);
-      Object.keys(req.body).map(function(i) {
-        var element = _.find(form, function(e) {
-          return e.name === i;
+    forms.findOne({business: ObjectID(business._id)}, function (err, form) {
+        if (err) {
+            return next(err);
+        }
+        if (!form) {
+            return next(new Error('Form for business not found: ' + business._id));
+        }
+
+        //Ensure that there are all the responses
+        var valid = _.every(form.fields, function (field, index) {
+            var name = '_' + index;
+            //TODO: Validation for dropdowns
+            return name in req.body && req.body[name].trim() !== '';
         });
-        if (element) formResponse.push({ label: element.label, value: req.body[i]});
-      });
 
-      // TODO: form response, add all of them to object, both Q and A
-      // may have to get the form JSON in db.forms, then match form element name/id with response, and save an array of objects to store { label: '', value: '' }
+        if (!valid) {
+            makeForm(db, req.params.id, req.body, function (formHtml) {
+                res.render('checkin/customform', {
+                    formHtml: formHtml,
+                    formError: 'You are missing required fields.',
+                    companyName: business.companyName,
+                    bg: business.style.bg,
+                    logo: business.logo,
+                    buttonBg: style.rgbObjectToCSS(business.style.buttonBg),
+                    buttonText: style.rgbObjectToCSS(business.style.buttonText),
+                    containerText: style.rgbObjectToCSS(business.style.containerText),
+                    containerBg: style.rgbObjectToCSS(business.style.containerBg)
+                });
+            });
+        } else { //Form is valid, let's put it into the DB
+            var formResponses = db.get('formResponses');
+            var formResponse = {
+                appointment: req.session.appointmentId,
+                answers: []
+            };
 
-      appointments.update({ _id: ObjectId(appointmentId) }, { $set: {
-        formResponse: formResponse
-      }});
+            _.each(form.fields, function (field, index) {
+                var name = '_' + index;
+                formResponse.answers.push({
+                    label: field.label,
+                    response: req.body[name]
+                });
+            });
 
-      res.redirect('sign');
+            formResponses.insert(formResponse, function (err) {
+                if (err) {
+                    return next(err);
+                }
+
+                //Update the state of the appointment
+                var appointmentId = req.session.appointmentId;
+                db.get('appointments').findAndModify({_id: appointmentId}, {
+                    $set: {
+                        state: 'formDone'
+                    }
+                }, function (err, appt) {
+                    if (err) {
+                        return next(err);
+                    }
+                    if (!appt) {
+                        return next(new Error('Appointment not found'));
+                    }
+
+                    //Find the employee this appointment belongs to
+                    db.get('employees').findById(appt.employee, function (err, employee) {
+                        if (err) {
+                            return next(err);
+                        }
+                        if (!employee) {
+                            return next(new Error('Employee not found'));
+                        }
+
+                        sms.sendText(employee.phone, 'Your patient ' + appt.fname + ' ' + appt.lname + ' has checked in.', function (err) {
+                            if (err) {
+                                return next(err);
+                            }
+
+                            res.redirect('sign');
+                        });
+                    });
+                });
+            });
+        }
     });
 };
